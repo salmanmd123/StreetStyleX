3e3da1e1a1f724e25716048da46682bd2fe5a035
salmanmd123 <salmanmd250705@gmail.com>
2026-04-18 20:26:59 +0530

    Fix import case for ProductItem component

--- frontend/src/pages/Kidswear.jsx
+++ frontend/src/pages/Kidswear.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/frontend_assets/assets';
 import Title from '../components/Title';
-import ProductItem from '../components/productitem';
+import ProductItem from '../components/Productitem';
 
 const Kidswear = () => {
   const { products, search, showSearch } = useContext(ShopContext);
