24ae66f2d3ae201cc6f1f48a61d4ce523e0ff74b
Mohammed Salman <salmanmd250705@gmail.com>
2026-04-19 10:54:20 +0530

    fix axios issue

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -110,7 +110,7 @@ const ShopContextProvider = (props) => {
     const getProductsData = async () => {
         try {
 
-            const response = await axios.get(backendUrl + '/api/product/list')
+            const response = await axios.get(backendUrl + 'api/product/list')
             if (response.data.success) {
                 setProducts(response.data.products)
             } else {
